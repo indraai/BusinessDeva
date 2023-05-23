ad98d919f30c44416429992fa0aa36bb82059fbf
Quinn <quinn@indra.ai>
2023-05-22 19:36:25 -0700

    prompt update

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ const package = require('./package.json');
 const info = {
   id: package.id,
   name: package.name,
-  version: package.version,
-  author: package.author,
   describe: package.description,
+  version: package.version,
   url: package.homepage,
   git: package.repository.url,
   bugs: package.bugs.url,
+  author: package.author,
   license: package.license,
-  copyright: package.copyright
+  copyright: package.copyright,
 };
 
 const data_path = path.join(__dirname, 'data.json');
